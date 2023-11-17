4cba1d24112a5ebd56c51ada67bb6252785134e2
asrasoft-admin <mohammadumer704@gmail.com>
2023-11-18 00:44:43 +0530

    Add show pass img

--- src/common/Input/index.js
+++ src/common/Input/index.js
@@ -64,7 +64,7 @@ export const Input = ({
               source={
                 !showPassword
                   ? require('../../assets/images/closeEye.png')
-                  : require('../../assets/images/closeEye.png')
+                  : require('../../assets/images/openEye.png')
               }
               style={style.showPassImg}
             />
