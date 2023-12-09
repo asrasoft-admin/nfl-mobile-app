fae6d36022fe7bd1b731e6018edc11d4345710e6
asrasoft-admin <asrasoft.managers@gmail.com>
2023-12-09 15:26:43 +0530

    prod branch

--- src/config/index.js
+++ src/config/index.js
@@ -3,8 +3,8 @@ import _ from 'lodash';
 
 import defaults from './stage';
 
-const APP_ENV = 'stage';
-// const APP_ENV = 'prod';
+// const APP_ENV = 'stage';
+const APP_ENV = 'prod';
 
 const envOverrides = require(`./${APP_ENV}`);
 export default _.merge({}, defaults, envOverrides);
--- src/services/cloudinary/Cloudinary.js
+++ src/services/cloudinary/Cloudinary.js
@@ -13,52 +13,52 @@ import {
 import config from '../../config';
 
 // This is for stagging -----
-const uploadAudioToCloudinary = async audioPath => {
-  // stage --
-  const url = config.cloudinaryURL.STAGE_CLOUDINARY_URL;
+// const uploadAudioToCloudinary = async audioPath => {
+//   // stage --
+//   const url = config.cloudinaryURL.STAGE_CLOUDINARY_URL;
 
-  const generatedFilename = audioPath.substring(audioPath.lastIndexOf('/') + 1);
+//   const generatedFilename = audioPath.substring(audioPath.lastIndexOf('/') + 1);
 
-  const uploadData = [
-    {
-      name: 'file',
-      filename: generatedFilename,
-      type: 'audio/aac',
-      data: RNFetchBlob.wrap(audioPath),
-    },
-    // stage --
-    {
-      name: 'upload_preset',
-      data: config.cloudinaryURL.STAGE_CLOUDINARY_DATA_STR,
-    },
-  ];
+//   const uploadData = [
+//     {
+//       name: 'file',
+//       filename: generatedFilename,
+//       type: 'audio/aac',
+//       data: RNFetchBlob.wrap(audioPath),
+//     },
+//     // stage --
+//     {
+//       name: 'upload_preset',
+//       data: config.cloudinaryURL.STAGE_CLOUDINARY_DATA_STR,
+//     },
+//   ];
 
-  console.log({uploadData});
-  try {
-    const response = await RNFetchBlob.fetch(
-      'POST',
-      url,
-      {
-        'Content-Type': 'multipart/form-data',
-      },
-      uploadData.map(field => ({
-        name: field.name,
-        filename: field.filename,
-        type: field.type,
-        data: field.data,
-      })),
-    );
+//   console.log({uploadData});
+//   try {
+//     const response = await RNFetchBlob.fetch(
+//       'POST',
+//       url,
+//       {
+//         'Content-Type': 'multipart/form-data',
+//       },
+//       uploadData.map(field => ({
+//         name: field.name,
+//         filename: field.filename,
+//         type: field.type,
+//         data: field.data,
+//       })),
+//     );
 
-    const responseJson = await response.json();
-    const downloadLink = responseJson.secure_url;
-    return downloadLink;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-};
+//     const responseJson = await response.json();
+//     const downloadLink = responseJson.secure_url;
+//     return downloadLink;
+//   } catch (error) {
+//     console.log(error);
+//     throw error;
+//   }
+// };
 
-export default uploadAudioToCloudinary;
+// export default uploadAudioToCloudinary;
 
 // cloudinary.config({
 //   cloud_name: 'dwhyqylgz',
@@ -68,43 +68,43 @@ export default uploadAudioToCloudinary;
 // });
 
 // This is for production -----
-// const uploadAudioToS3 = async audioPath => {
-//   // Set up AWS configuration
-//   AWS.config.update({
-//     region: config.S3_URL.PROD_S3_REGION, // e.g., 'us-east-1'
-//     accessKeyId: config.S3_URL.PROD_S3_ACCESS_KEY_ID,
-//     secretAccessKey: config.S3_URL.PROD_S3_SECRET_ACCESS_KEY,
-//     correctClockSkew: true,
-//   });
+const uploadAudioToS3 = async audioPath => {
+  // Set up AWS configuration
+  AWS.config.update({
+    region: config.S3_URL.PROD_S3_REGION, // e.g., 'us-east-1'
+    accessKeyId: config.S3_URL.PROD_S3_ACCESS_KEY_ID,
+    secretAccessKey: config.S3_URL.PROD_S3_SECRET_ACCESS_KEY,
+    correctClockSkew: true,
+  });
 
-//   const s3 = new AWS.S3();
+  const s3 = new AWS.S3();
 
-//   // Generate a unique filename (you can modify this logic)
-//   const generatedFilename = audioPath.substring(audioPath.lastIndexOf('/') + 1);
+  // Generate a unique filename (you can modify this logic)
+  const generatedFilename = audioPath.substring(audioPath.lastIndexOf('/') + 1);
 
-//   // Read the audio file as a Buffer
-//   const fileContent = await RNFetchBlob.fs.readFile(audioPath, 'base64');
-//   const fileBuffer = Buffer.from(fileContent, 'base64');
+  // Read the audio file as a Buffer
+  const fileContent = await RNFetchBlob.fs.readFile(audioPath, 'base64');
+  const fileBuffer = Buffer.from(fileContent, 'base64');
 
-//   // S3 Upload parameters
-//   const params = {
-//     Bucket: PROD_S3_BUCKET,
-//     Key: `uploads/${generatedFilename}`,
-//     Body: fileBuffer,
-//     ContentType: 'audio/aac', // Adjust based on your actual audio file type
-//   };
+  // S3 Upload parameters
+  const params = {
+    Bucket: PROD_S3_BUCKET,
+    Key: `uploads/${generatedFilename}`,
+    Body: fileBuffer,
+    ContentType: 'audio/aac', // Adjust based on your actual audio file type
+  };
 
-//   try {
-//     // Upload to S3
-//     const response = await s3.upload(params).promise();
+  try {
+    // Upload to S3
+    const response = await s3.upload(params).promise();
 
-//     const publicUrl = response.Location;
-//     console.log('Successfully uploaded to S3:', publicUrl);
-//     return publicUrl;
-//   } catch (error) {
-//     console.error('Error uploading to S3:', error);
-//     throw new Error('Failed to upload to S3');
-//   }
-// };
+    const publicUrl = response.Location;
+    console.log('Successfully uploaded to S3:', publicUrl);
+    return publicUrl;
+  } catch (error) {
+    console.error('Error uploading to S3:', error);
+    throw new Error('Failed to upload to S3');
+  }
+};
 
-// export default uploadAudioToS3;
+export default uploadAudioToS3;
