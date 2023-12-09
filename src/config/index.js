83f223fb246aa3d8e462d6c30ca7e7b099ebca1e
asrasoft-admin <asrasoft.managers@gmail.com>
2023-12-09 15:57:18 +0530

    Code cleanup

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,7 @@
 import _ from 'lodash';
-// import {APP_ENV} from '@env';
 
 import defaults from './stage';
 
-// const APP_ENV = 'stage';
 const APP_ENV = 'prod';
 
 const envOverrides = require(`./${APP_ENV}`);
--- src/services/cloudinary/Cloudinary.js
+++ src/services/cloudinary/Cloudinary.js
@@ -1,71 +1,8 @@
 import RNFetchBlob from 'react-native-fetch-blob';
 import AWS from 'aws-sdk';
 import {Buffer} from 'buffer';
-import {
-  STAGE_CLOUDINARY_URL,
-  STAGE_CLOUDINARY_DATA_STR,
-  PROD_S3_REGION,
-  PROD_S3_ACCESS_KEY_ID,
-  PROD_S3_SECRET_ACCESS_KEY,
-  PROD_S3_BUCKET,
-} from '@env';
 
 import config from '../../config';
-
-// This is for stagging -----
-// const uploadAudioToCloudinary = async audioPath => {
-//   // stage --
-//   const url = config.cloudinaryURL.STAGE_CLOUDINARY_URL;
-
-//   const generatedFilename = audioPath.substring(audioPath.lastIndexOf('/') + 1);
-
-//   const uploadData = [
-//     {
-//       name: 'file',
-//       filename: generatedFilename,
-//       type: 'audio/aac',
-//       data: RNFetchBlob.wrap(audioPath),
-//     },
-//     // stage --
-//     {
-//       name: 'upload_preset',
-//       data: config.cloudinaryURL.STAGE_CLOUDINARY_DATA_STR,
-//     },
-//   ];
-
-//   console.log({uploadData});
-//   try {
-//     const response = await RNFetchBlob.fetch(
-//       'POST',
-//       url,
-//       {
-//         'Content-Type': 'multipart/form-data',
-//       },
-//       uploadData.map(field => ({
-//         name: field.name,
-//         filename: field.filename,
-//         type: field.type,
-//         data: field.data,
-//       })),
-//     );
-
-//     const responseJson = await response.json();
-//     const downloadLink = responseJson.secure_url;
-//     return downloadLink;
-//   } catch (error) {
-//     console.log(error);
-//     throw error;
-//   }
-// };
-
-// export default uploadAudioToCloudinary;
-
-// cloudinary.config({
-//   cloud_name: 'dwhyqylgz',
-//   api_key: '566327318464425',
-
-//   api_secret: 'DmPiE1xCKoMjPg3QeQiFv1VTAMk',
-// });
 
 // This is for production -----
 const uploadAudioToS3 = async audioPath => {
@@ -88,7 +25,7 @@ const uploadAudioToS3 = async audioPath => {
 
   // S3 Upload parameters
   const params = {
-    Bucket: PROD_S3_BUCKET,
+    Bucket: config.PROD_S3_BUCKET,
     Key: `uploads/${generatedFilename}`,
     Body: fileBuffer,
     ContentType: 'audio/aac', // Adjust based on your actual audio file type
